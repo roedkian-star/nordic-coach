94191dd56e00ccbfb5d3b83afae456fe18b4d44c
roedkian-star <roed.kian@gmail.com>
2026-03-15 18:37:20 +0100

    Update tailwind.config.js

--- nordic-coach/tailwind.config.js
+++ nordic-coach/tailwind.config.js
@@ -1,11 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
+export default config;
